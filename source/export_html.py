3227c05deb4497199c76dbf73233debb85b9f0ba
Grimscythe1983 <pjdamian.chrzanowski@gmail.com>
2016-04-10 23:13:19 +0100

    Adding file for exporting as html

---
+++ source/export_html.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+# -----------------------------------------------------------------------------
+# Name:        export_html.py
+# Purpose:     JDF database editor
+# Author:      Damian Chrzanowski
+# Created:     22/03/16
+# Modified:    06/04/16
+# Copyright:   pjdamian.chrzanowski@gmail.com
+# License:     GNU Public License v3
+# Version:     1.0
+# Revision:    N/A
+# -----------------------------------------------------------------------------
+# jdf_editor, GUI application for editing database files,
+#             specifically designed for the new JDF file format.
+#             (J)son(D)atabase(F)ormat
+# Copyright (C) 2016 Damian Chrzanowski
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------
